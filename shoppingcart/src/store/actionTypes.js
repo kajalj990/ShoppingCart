560a79837b3585fc3904bdf9231b47ed85adfc1b
Kajal Jaiswal <kajalj990@gmail.com>
2020-08-28 14:03:37 +0530

    cart again changes

--- shoppingcart/src/store/actionTypes.js
+++ shoppingcart/src/store/actionTypes.js
@@ -3,3 +3,5 @@ export const AUTH_LOGOUT = 'LOGOUT';
 export const AUTH_ERROR = 'AUTH_ERRO';
 export const AUTH_REGISTER = 'REGISTER';
 export const GET_USER = 'USER'
+export const GET_CART = 'CART'
+export const CART = 'NEW_CART'
