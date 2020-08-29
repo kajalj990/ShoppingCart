8228f60aa0313bf743aa9de0e841f5fad7305d85
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-08-30 00:48:02 +0530

    Fixed indentation

--- order/api/routes/orderController.js
+++ order/api/routes/orderController.js
@@ -1,70 +1,79 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const orderModel = require('../model/orderModel')
-const router = express.Router()
-const Order = require('../model/orderModel')
-const axios = require('axios')
-router.post('/',(req,res)=>{
-    const newOrder = new Order({
-        _id: mongoose.Types.ObjectId(),
-        cartId:mongoose.Types.ObjectId(req.body.cartId),
-        orderdDate:Date.now()
-    })
-    newOrder.save().then(order=>{
-    axios.get('http://localhost:3004/cart/'+req.body.cartId).then((currentCart)=>{
-    const prodlist = [];
-    console.log(currentCart.data);
-    currentCart.data.cart.productList.forEach((item) => {
-      currentProduct = {
-        id: item.productId,
-        quantity: +item.quantity,
-      };
-      prodlist.push(currentProduct);
-    });
-    axios
-      .post('http://localhost:3002/products/update', {products: prodlist})
-      .then((result) => {
-        // Calculate your Totla Payment here
-        res.json({
-            order:order,
-          result: 'Sucessfully Create a purchase'
-        });   
-        })
-    //     res.status(200).json({
-    //         order:order,
-    //         message:"Your order has been placed "
-         })
-     }).catch(err=>{
-         res.json({
-             error:err
-         })
-     })
-})
-
-router.get('/:_id',(req,res)=>{
-    Order.findById(req.params._id).exec()
-    .then(order=>{
-         axios.get('http://localhost:3004/cart/'+ order.cartId).then(obj=>{
-            
-            console.log(obj)
-            res.json({
-                order:{orderId:order._id,cart:obj.data.cart}
+const express = require('express');
+const mongoose = require('mongoose');
+const orderModel = require('../model/orderModel');
+const router = express.Router();
+const Order = require('../model/orderModel');
+const axios = require('axios');
+router.post('/', (req, res) => {
+  const newOrder = new Order({
+    _id: mongoose.Types.ObjectId(),
+    cartId: mongoose.Types.ObjectId(req.body.cartId),
+    orderdDate: Date.now(),
+  });
+  newOrder
+    .save()
+    .then((order) => {
+      axios
+        .get('http://localhost:3004/cart/' + req.body.cartId)
+        .then((currentCart) => {
+          const prodlist = [];
+          console.log(currentCart.data);
+          currentCart.data.cart.productList.forEach((item) => {
+            currentProduct = {
+              id: item.productId,
+              quantity: +item.quantity,
+            };
+            prodlist.push(currentProduct);
+          });
+          axios
+            .post('http://localhost:3002/products/update', {
+              products: prodlist,
             })
+            .then((result) => {
+              // Calculate your Totla Payment here
+              res.json({
+                order: order,
+                result: 'Sucessfully Create a purchase',
+              });
+            });
+          //     res.status(200).json({
+          //         order:order,
+          //         message:"Your order has been placed "
+        });
     })
-    }).catch(err=>{
-        res.json({
-            error : err
-        })
-    })
-})
+    .catch((err) => {
+      res.json({
+        error: err,
+      });
+    });
+});
 
-router.delete('/cancelOrder/:orderId',(req,res)=>{
-    Order.findById(req.params.orderId).exec()
-    .then(()=>{
+router.get('/:_id', (req, res) => {
+  Order.findById(req.params._id)
+    .exec()
+    .then((order) => {
+      axios.get('http://localhost:3004/cart/' + order.cartId).then((obj) => {
+        console.log(obj);
         res.json({
-            message : "your order has been successfully deleted"
-        })
+          order: { orderId: order._id, cart: obj.data.cart },
+        });
+      });
     })
-})
+    .catch((err) => {
+      res.json({
+        error: err,
+      });
+    });
+});
+
+router.delete('/cancelOrder/:orderId', (req, res) => {
+  Order.findById(req.params.orderId)
+    .exec()
+    .then(() => {
+      res.json({
+        message: 'your order has been successfully deleted',
+      });
+    });
+});
 
 module.exports = router;
